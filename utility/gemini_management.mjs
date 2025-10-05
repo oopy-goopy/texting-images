e256b39e63412b42765c16db35949b705ee09356
Willywum <will.ch2705@gmail.com>
2025-10-05 06:05:20 -0700

    the user

--- utility/gemini_management.mjs
+++ utility/gemini_management.mjs
@@ -11,6 +11,7 @@ export async function describe(user, arr, lang) {
     Out of the following array ${arr}, form a single sentence trying to assume what ${user} is trying to say.
     Output only the sentence and nothing else.
     Try to interpret the emotion the array is trying to convey in the sentence.
+    Refrain from ever saying the words "the user" and only refer to them as ${user}.
     Note: The words in the array does not need to be in the sentence.
     Language: ${lang}`;
     const response = await ai.models.generateContent({
