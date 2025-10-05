2ea597880e43f0e896c2b82ba81980db92a44d97
Willywum <will.ch2705@gmail.com>
2025-10-05 04:13:41 -0700

    a great big tree added as condition on message

--- server.mjs
+++ server.mjs
@@ -42,6 +42,7 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     const user = users[socket.id];
     if (user && user.room) {
+        if (msg == "a great big tree") {describe(["tree", "big", "great"], "English") = msg}
       const payload = { user: user.username, text: msg };
       io.to(user.room).emit('chat message', payload);
       roomMessages[user.room].push(payload);
