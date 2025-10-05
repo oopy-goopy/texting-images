327b10a6f3fb1a94bccc623d0e9b7b1fe386db22
Willywum <will.ch2705@gmail.com>
2025-10-05 05:58:44 -0700

    emotion

--- utility/gemini_management.mjs
+++ utility/gemini_management.mjs
@@ -8,8 +8,9 @@ const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API});
 
 export async function describe(user, arr, lang) {
     const prompt = `
-    Out of the following array ${arr}, form a sentence trying to assume what ${user} is trying to say.
+    Out of the following array ${arr}, form a single sentence trying to assume what ${user} is trying to say.
     Output only the sentence and nothing else.
+    Try to interpret the emotion the array is trying to convey in the sentence.
     Note: The words in the array does not need to be in the sentence.
     Language: ${lang}`;
     const response = await ai.models.generateContent({
